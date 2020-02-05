07aed0d71235e65d1113fe09e044954da382f629
Willmer Barahona <wbarahona@live.com>
2020-02-05 16:33:25 -0600

    Quickie fix to comply route validation
    Thou shall not return an improper response to the route. See route schema.

--- src/modules/items/itemsHandler.ts
+++ src/modules/items/itemsHandler.ts
@@ -7,15 +7,11 @@ const all = (request: object, h: ResponseToolkit) => {
   // const { from, limit } = query;
 
   let statusCode = 500;
-  let code = 0;
-  let message = '';
   let content = {};
 
   try {
     // call to some service is mocked here this must be a async method that awaits service call or method
     statusCode = 200;
-    code = 1;
-    message = 'some message';
     content = [
       {
         name: 'pepe',
@@ -29,15 +25,11 @@ const all = (request: object, h: ResponseToolkit) => {
       },
     ];
   } catch (err) {
-    message = `Items handler "all" operation was unsuccessful: ${err.message}`;
     content = err;
   }
 
   return h
     .response({
-      statusCode,
-      code,
-      message,
       content,
     })
     .code(statusCode);
