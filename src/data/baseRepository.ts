ad68cc3b8b3f3fd00f0198bd5d1f6c5554b8dfb0
Christopher Escalon <escalonchristopher@gmail.com>
2020-02-06 15:33:58 -0600

    updating models

--- src/data/baseRepository.ts
+++ src/data/baseRepository.ts
@@ -1,5 +1,5 @@
 export default interface BaseRepository<TShow, TCreate, TUpdate> {
-  all(): Promise<TShow>;
+  all(): Promise<TShow[]>;
   add(entity: TCreate): Promise<TCreate>;
   update(entity: TUpdate): Promise<TUpdate>;
   delete(id: number): Promise<void>;
